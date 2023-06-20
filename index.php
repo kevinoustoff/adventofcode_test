3643e8cbda2603d4bbfc34d79e391365a007ef5a
kevinoustoff <35457309+kevinoustoff@users.noreply.github.com>
2023-06-20 10:02:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "functions.php";
+//Test de l'algorithme
 $candidateBoxId = "aabcda";
 $candidatesBoxesIds = ["abcdef","bababc","abbcde","abcccd","aabcdd","abcdee","ababab"];
 echo "The checksum is : ".computeChecksum($candidatesBoxesIds);
